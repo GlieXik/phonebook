cee46fe480a81393206653258a6cf60a21ccdd82
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-03-06 17:43:37 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="goit-react-hw-08-phonebook">
+        <BrowserRouter basename="phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
